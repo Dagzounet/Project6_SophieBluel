775ff81b240390a2c683b59f47503c042ee59858
Dagzounet <paul.gamot@gmail.com>
2023-07-17 03:16:54 +0200

    Rework of modal.js to wrap everything inside different function
    Rework of modal.js to wrap everything inside different function

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -1,33 +1,20 @@
 // Partie ouverture, fermeture et gestion des événements de la modale
 
+// Récupération de la modal du html
+const modal = document.querySelector('.modal');
+const modalWrapper2 = document.getElementById('modal2');
 
 // Fonction d'ouverture de la modale
-const openModal = function (e) {
+function openModal(e) {
   e.preventDefault();
   const target = document.querySelector(e.target.getAttribute('href'));
   target.style.display = 'flex';
   target.removeAttribute('aria-hidden');
   target.setAttribute('aria-modal', 'true');
-};
-
-// Fonction du bouton "modifier"
-const addEditButtonEventListener = (button) => {
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
-    const modalWrapper = document.querySelector('.modal-wrapper');
-    const modalWrapper2 = document.getElementById('modal2');
-
-    modalWrapper2.style.display = 'none';
-    modalWrapper.style.display = 'block';
-    const target = document.querySelector(e.target.getAttribute('href'));
-    target.style.display = 'flex';
-    target.removeAttribute('aria-hidden');
-    target.setAttribute('aria-modal', 'true');
-  });
-};
+}
 
 // Fonction pour fermer la modal
-const closeModal = () => {
+function closeModal() {
   modal.style.display = 'none';
   modal.setAttribute('aria-hidden', 'true');
   modal.removeAttribute('aria-modal');
@@ -35,34 +22,29 @@ const closeModal = () => {
   img.src = '';
   // Réinitialise la valeur du file input
   fileInput.value = null;
-
   // Réinitialise le texte dans "Titre"
   titleInput.value = '';
-
   // Réinitialise le texte dans "Catégorie"
   categorySelect.selectedIndex = 0;
-
   // Supprime le message d'erreur s'il est présent
   const existingErrorMessage = modal2.querySelector('.errorMessage');
   if (existingErrorMessage) {
     existingErrorMessage.remove();
   }
-};
+}
 
 // Ajout evenement pour refermer modal lors d'un clic externe
-const handleModalClick = (e) => {
+function handleModalClick(e) {
   if (e.target === modal) {
     closeModal();
     // Réinitialise l'image en aperçu
     img.src = '';
-
     // Réinitialise le texte dans "Titre"
     titleInput.value = '';
-
     // Réinitialise le texte dans "Catégorie"
     categorySelect.selectedIndex = 0;
   }
-};
+}
 
 
 
@@ -70,7 +52,7 @@ const handleModalClick = (e) => {
 // Partie suppression d'une image de l'interface utilisateur et de l'API
 
 // Fonction pour supprimer une image de l'interface utilisateur
-const deleteImageFromUI = (imageId) => {
+function deleteImageFromUI(imageId) {
   // Supprime l'image de la section "portfolio"
   const portfolioImageElement = document.querySelector(`#portfolio [data-image-id="${imageId}"]`);
   if (portfolioImageElement) {
@@ -82,10 +64,10 @@ const deleteImageFromUI = (imageId) => {
   if (modalImageElement) {
     modalImageElement.remove();
   }
-};
+}
 
 // Fonction pour supprimer une image
-const deleteImage = (imageId) => {
+function deleteImage(imageId) {
   const deleteUrl = `http://localhost:5678/api/works/${imageId}`;
 
   // Récupère le token à partir du local storage
@@ -107,16 +89,15 @@ const deleteImage = (imageId) => {
     .catch(error => {
       console.error('Erreur lors de la suppression de l\'image :', error);
     });
-};
+}
 
 
 
 
 // Partie d'ajout de la galerie à la modal
 
-
 // Fonction pour ajouter la galerie à container
-const addGalleryToContainer = (data) => {
+function addGalleryToContainer(data) {
   const galleryContainer = document.querySelector('.gallery-container');
 
   data.forEach(item => {
@@ -154,7 +135,7 @@ const addGalleryToContainer = (data) => {
   icon.classList.add('fa-solid', 'fa-arrows-up-down-left-right');
 
   firstFigure.appendChild(icon);
-};
+}
 
 
 
@@ -162,75 +143,91 @@ const addGalleryToContainer = (data) => {
 // Partie Gestion des événements et création des éléments dans le DOM
 
 
-// Sélection de tous les boutons "Modifier" avec ajout d'événement
-const editButtons = document.querySelectorAll('.edit_button');
-editButtons.forEach((button) => {
-  addEditButtonEventListener(button);
-});
+// Fonction pour ajouter l'événement de clic au bouton "Modifier"
+function addEditButtonEventListener(button) {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const modalWrapper = document.querySelector('.modal-wrapper');
 
-// Sélection du bouton "Fermer" de la modal et ajout d'événement pour masquer
-const closeButton = document.querySelector('.close_button');
-closeButton.addEventListener('click', closeModal);
-
-const modal = document.querySelector('.modal');
-
-// Ajout d'événement pour refermer la modal lors d'un clic externe
-modal.addEventListener('click', handleModalClick);
-
-const modalWrapper = document.querySelector('.modal-wrapper');
-
-// Création de l'élément h1
-const title = document.createElement('h1');
-title.textContent = 'Galerie Photo';
-
-// Insertion du titre en premier dans modal-wrapper
-modalWrapper.insertBefore(title, modalWrapper.firstChild);
-
-// Création de la ligne grise horizontale
-const hr = document.createElement('hr');
-modalWrapper.appendChild(hr);
-
-// Création du bouton "Ajouter une photo"
-const addButton = document.createElement('button');
-addButton.textContent = 'Ajouter une photo';
-addButton.classList.add('add-button');
-modalWrapper.appendChild(addButton);
-
-// Événement du bouton "Ajouter une photo"
-const modalWrapper2 = document.getElementById('modal2');
-
-addButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  modalWrapper.style.display = 'none';
-  modalWrapper2.style.display = 'block';
-});
-
-// Création du texte "Supprimer la galerie"
-const deleteText = document.createElement('p');
-deleteText.textContent = 'Supprimer la galerie';
-deleteText.classList.add('deletewholegallery');
-modalWrapper.appendChild(deleteText);
-
-
-
-
-//Partie Appel à l'API pour récupérer les données de la galerie
-
-fetch(url, {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Erreur : ' + response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    addGalleryToContainer(data); // Appel de la fonction pour ajouter la galerie
-  })
-  .catch(error => {
-    console.error('Erreur lors de l\'appel à l\'API :', error);
+    modalWrapper2.style.display = 'none';
+    modalWrapper.style.display = 'block';
+    const target = document.querySelector(e.target.getAttribute('href'));
+    target.style.display = 'flex';
+    target.removeAttribute('aria-hidden');
+    target.setAttribute('aria-modal', 'true');
   });
+}
+
+
+// Fonction pour initialiser les événements et créer les éléments dans le DOM
+function initializeModalEventsAndElements() {
+  const editButtons = document.querySelectorAll('.edit_button');
+  editButtons.forEach((button) => {
+    addEditButtonEventListener(button);
+  });
+
+  const closeButton = document.querySelector('.close_button');
+  closeButton.addEventListener('click', closeModal);
+
+  const modal = document.querySelector('.modal');
+  modal.addEventListener('click', handleModalClick);
+
+  const modalWrapper = document.querySelector('.modal-wrapper');
+
+  const title = document.createElement('h1');
+  title.textContent = 'Galerie Photo';
+
+  modalWrapper.insertBefore(title, modalWrapper.firstChild);
+
+  const hr = document.createElement('hr');
+  modalWrapper.appendChild(hr);
+
+  const addButton = document.createElement('button');
+  addButton.textContent = 'Ajouter une photo';
+  addButton.classList.add('add-button');
+  modalWrapper.appendChild(addButton);
+
+  addButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    modalWrapper.style.display = 'none';
+    modalWrapper2.style.display = 'block';
+  });
+
+  const deleteText = document.createElement('p');
+  deleteText.textContent = 'Supprimer la galerie';
+  deleteText.classList.add('deletewholegallery');
+  modalWrapper.appendChild(deleteText);
+}
+
+
+
+
+// Partie Appel à l'API pour récupérer les données de la galerie
+function fetchGalleryData(url) {
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erreur : ' + response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      addGalleryToContainer(data); // Appel de la fonction pour ajouter la galerie
+    })
+    .catch(error => {
+      console.error('Erreur lors de l\'appel à l\'API :', error);
+      const errorServer = createErrorMessage('Le serveur n\'est pas allumé');
+      modal.insertBefore(errorServer, modal.firstChild);
+    });
+}
+
+// Appel pour initialiser les événements et créer les éléments dans le DOM
+initializeModalEventsAndElements();
+
+// Appel à l'API pour récupérer les données de la galerie
+fetchGalleryData(url);
